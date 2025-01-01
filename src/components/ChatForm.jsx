e1161cd930265445c6bb969a6214d523300270a0
Noskina Olha <noskinaolga1@gmail.com>
2025-01-01 17:46:35 +0100

    Add ChatForm component with message input and submit functionality

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,8 +1,19 @@
+import {useRef} from "react";
 
 const ChatForm = () => {
+    const inputRef = useRef();
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const userMessage = inputRef.current.value.trim();
+        if (!userMessage) return;
+
+        console.log(userMessage);
+        inputRef.current.value = '' //Clearing the input field after sending a message
+    }
+
     return (
-        <form action='#' className='chat-form'>
-            <input type='text' placeholder='Your message here...' className='message-input' required/>
+        <form action='#' className='chat-form' onSubmit={handleFormSubmit}>
+            <input ref={inputRef} type='text' placeholder='Your message here...' className='message-input' required/>
             <button className="material-symbols-outlined">
                 arrow_upward
             </button>
